4ade624a27afa66c14861aa33500c71fe6c53bfd
Aaron Villalpando <aaron.vlpn@gmail.com>
2023-08-29 01:32:38 -0700

    some commit

--- Formula/gloo.rb
+++ Formula/gloo.rb
@@ -2,8 +2,8 @@ class Gloo < Formula
   desc "Gloo Utilities" 
   homepage "https://www.trygloo.com"
   url "https://github.com/GlooHQ/homebrew-gloo/releases/download/v0.1.0/gloo_cli.tar.gz"
-  sha256 "f05a325ea9965781016fd2e5a7c62970cf4633b3c116ffb679c02fc570338b63"
-  version "0.1.0"
+  sha256 "4450c8f923103c9b0e0dfcc2e3f47dc9f06b13eb6bea6f011ce17edc478441b6"
+  version "0.1.1"
 
   def install
     bin.install "gloo_cli"
