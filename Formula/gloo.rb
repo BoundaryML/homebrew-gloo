9dbc8f29e12dcac873b12037339606f812a4c3e6
Aaron Villalpando <aaron.vlpn@gmail.com>
2023-08-28 23:13:10 -0700

    Add gloo formula

---
+++ Formula/gloo.rb
@@ -0,0 +1,11 @@
+class Rustscan < Formula
+  desc "Gloo Utilities" 
+  homepage "https://www.trygloo.com"
+  url "https://github.com/GlooHQ/homebrew-gloo/releases/download/v0.1.0/gloo-0.1.0-x64_64-apple-darwin.tar.gz"
+  sha256 "860f3c3f257dfbed2189ceacfaf4e08882bd3f1c8331c51aa0e0fe24abca9c10"
+  version "0.1.0"
+
+  def install
+    bin.install "gloo"
+  end
+end
