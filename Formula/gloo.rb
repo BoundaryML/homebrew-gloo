a05411f7bfb1eb21c34c471a94a65f6e3e498a17
Aaron Villalpando <aaron.vlpn@gmail.com>
2023-08-29 01:34:07 -0700

    Add new release url

--- Formula/gloo.rb
+++ Formula/gloo.rb
@@ -1,7 +1,7 @@
 class Gloo < Formula
   desc "Gloo Utilities" 
   homepage "https://www.trygloo.com"
-  url "https://github.com/GlooHQ/homebrew-gloo/releases/download/v0.1.0/gloo_cli.tar.gz"
+  url "https://github.com/GlooHQ/homebrew-gloo/releases/download/0.1.1/gloo_cli.tar.gz"
   sha256 "4450c8f923103c9b0e0dfcc2e3f47dc9f06b13eb6bea6f011ce17edc478441b6"
   version "0.1.1"
 
