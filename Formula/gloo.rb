04be34fa99affdd2b8d928b3e93b001469a79e4d
Aaron Villalpando <aaron.vlpn@gmail.com>
2023-09-01 12:50:05 -0700

    release 0.1.6

--- Formula/gloo.rb
+++ Formula/gloo.rb
@@ -1,9 +1,9 @@
 class Gloo < Formula
   desc "Gloo Utilities" 
   homepage "https://www.trygloo.com"
-  url "https://github.com/GlooHQ/homebrew-gloo/releases/download/0.1.1/gloov0.1.5.tar.gz"
-  sha256 "aa17ec109f1ea3e2b31400622035b56a1eee82da5834e28cb641a579ddb91287"
-  version "0.1.5"
+  url "https://github.com/GlooHQ/homebrew-gloo/releases/download/0.1.1/gloov0.1.6.tar.gz"
+  sha256 "4e5849a3c5891178d153d00f0f31bf87b4be57ab6a859addc52dd1b923f9639f"
+  version "0.1.6"
 
   def install
     bin.install "gloo"
