44d140739bea11a78b18c88d7acddb5236113d1d
aaronvg <aaron.vlpn@gmail.com>
2023-09-05 23:54:23 -0700

    Update latest release 0.2

--- Formula/gloo.rb
+++ Formula/gloo.rb
@@ -1,9 +1,16 @@
 class Gloo < Formula
   desc "Gloo Utilities" 
   homepage "https://www.trygloo.com"
-  url "https://github.com/GlooHQ/homebrew-gloo/releases/download/v0.2/gloo-silicon.tar.gz"
-  sha256 "ac578b3f1f7a4382b4dee406dd4584d1608f367db8d3e74d28c00246054db104"
   version "0.2"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/GlooHQ/homebrew-gloo/releases/download/0.1.1/gloo-apple-intel.tar.gz"
+      sha256 "dfb02440c595e1029e74f19c6d0e361c5e5bec7fe581485b6cc8134113d0d438"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/GlooHQ/homebrew-gloo/releases/download/v0.2/gloo-apple-arm.tar.gz"
+      sha256 "ac578b3f1f7a4382b4dee406dd4584d1608f367db8d3e74d28c00246054db104"
+    end
 
   def install
     bin.install "gloo"
