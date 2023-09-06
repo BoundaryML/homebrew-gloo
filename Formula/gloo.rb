42c2b54456ce7e577ce9b437b2c423b6c2eac751
aaronvg <aaron.vlpn@gmail.com>
2023-09-05 23:54:45 -0700

    fix version on apple itnel

--- Formula/gloo.rb
+++ Formula/gloo.rb
@@ -5,7 +5,7 @@ class Gloo < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/GlooHQ/homebrew-gloo/releases/download/0.1.1/gloo-apple-intel.tar.gz"
+      url "https://github.com/GlooHQ/homebrew-gloo/releases/download/v0.2/gloo-apple-intel.tar.gz"
       sha256 "dfb02440c595e1029e74f19c6d0e361c5e5bec7fe581485b6cc8134113d0d438"
     elsif Hardware::CPU.arm?
       url "https://github.com/GlooHQ/homebrew-gloo/releases/download/v0.2/gloo-apple-arm.tar.gz"
