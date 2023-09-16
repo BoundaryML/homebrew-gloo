cf693f654b6546bd3eaf69ff1b5cc3d41da7913c
Aaron Villalpando <aaron.vlpn@gmail.com>
2023-09-16 09:46:06 -0700

    Fix hashes for 0.2.2

--- Formula/gloo.rb
+++ Formula/gloo.rb
@@ -6,7 +6,7 @@ class Gloo < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/GlooHQ/homebrew-gloo/releases/download/v0.2.2/gloo-apple-intel.tar.gz"
-      sha256 "7754b6c5b2063242638126cede5b1656185bf047d8e7981f86033cb9b060af71"
+      sha256 "1cdad0efa5b2334a67960160efa7f30ff78ddc807eb01bd94d99f10656f828be"
     elsif Hardware::CPU.arm?
       url "https://github.com/GlooHQ/homebrew-gloo/releases/download/v0.2.2/gloo-apple-arm.tar.gz"
       sha256 "e547b2021230240ef907e46c96c79a9c7487c78cfd16b0668a6fd80935db329e"
